ba14d770cd4b7ce0f9a916e31cbe362d882b02e4
STIVEN MORVAN <stiven.morvan@gmail.com>
2019-03-13 16:51:12 +0100

    Add a colorGradient function

--- generator/tools.js
+++ generator/tools.js
@@ -1,4 +1,11 @@
 module.exports = {
+  colorGradient : function (color1, color2, percentage) {
+      var percentage2 = 1 - p1;
+      var rgb = [Math.round(color1.r * p1 + color2.r * percentage2),
+          Math.round(color1.g * p1 + color2.g * percentage2),
+          Math.round(color1.b * p1 + color2.b * percentage2)];
+      return rgb;
+  },
   // See this stackoverflow comment : https://stackoverflow.com/a/5624139/9672414
   hexToRgb : function (hex) {
       // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
