7dfd1b2914cd4d8a4767f87b58892c51dae92401
STIVEN MORVAN <stiven.morvan@gmail.com>
2019-03-13 17:54:51 +0100

    Debug and add the credits

--- generator/index.js
+++ generator/index.js
@@ -1,29 +1,27 @@
-let credits = `
-// -----------------------------------------------------------------------------
-// Generator for the css-gradient-animation project
-// Git : https://github.com/morvan-s/css-gradient-animation/tree/master
-// Author : Stiven Morvan
-// -----------------------------------------------------------------------------
+"use strict";
+var fs = require('fs');
+var tools = require('./tools');
+let credits =
+`/*-----------------------------------------------------------------------------
+-- Css-gradient-animation project
+-- Git : https://github.com/morvan-s/css-gradient-animation/tree/master
+-- Author : Stiven Morvan
+------------------------------------------------------------------------------*/
 `;
 
-"use strict";
-let fs = require('fs');
-let tools = require('./tools');
+let color1 = '#3f5efb';
+let color2 = '#fa3636';
 
-let a = '#3f5efb';
-let b = '#fa3636';
-let file = `generated_gradient/gradient_${a}_${b}.css`;
+let file = `generated_gradient/gradient_${color1}_${color2}.css`;
+color1 = tools.hexToRgb(color1);
+color2 = tools.hexToRgb(color2);
 
-a = tools.hexToRgb(a);
-b = tools.hexToRgb(b);
-
-
-if(a !== null && b !== null){
+if(color1 !== null && color2 !== null){
   let precision = 1;
   let cssAnimationEvolution = '';
   for (var i = 0; i <= 100; i += precision) {
     cssAnimationEvolution += '  ' + i + '%{--gradientColor:'
-        + tools.colorGradient(a,b,i/100) + ';}\n';
+        + tools.colorGradient(color1,color2,i/100) + ';}\n';
   }
 
   let compatibilityKeyframes = ['@-webkit-keyframes GradientColor {',
@@ -31,7 +29,8 @@ if(a !== null && b !== null){
     '@-o-keyframes GradientColor {',
     '@keyframes GradientColor {'];
 
-  let cssContent = compatibilityKeyframes.reduce((acc, current, id) => {
+  let cssContent = credits;
+  cssContent += compatibilityKeyframes.reduce((acc, current, id) => {
     if(id === 1) acc += '\n' + cssAnimationEvolution + '}\n';
     acc += '\n' + current + '\n' + cssAnimationEvolution + '}\n';
     return acc;
@@ -39,7 +38,7 @@ if(a !== null && b !== null){
 
   fs.writeFile(file, cssContent, function(err, data) {
     if (err) console.log(err);
-    console.log('Success : Gradient animation file written. ',file);
+    console.log('Success : Gradient animation file has been written. ',file);
   });
 } else {
   console.log('Error : Wrong parameters');
